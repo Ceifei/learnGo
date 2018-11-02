585d3a67ff25acb4873d0010ef8f09cde8e55a27
Ceifei <ceifei@qq.com>
2018-11-02 17:27:38 +0800

    添加1-1和1-2

---
+++ ch1/1.go
@@ -0,0 +1,39 @@
+package ch1
+
+//F1 knowledges about Program Structure in Go
+func F1() {
+
+	println()
+
+	//1-1: Keywords
+	func() {
+		println("1-1 关键字")
+		println("----------------")
+		println("Go一共有25个关键字，按照用途可以分为以下三类：")
+		println("程序声明(2)		import,package")
+		println("实体声明(8)		chan,const,func,interface,map,struct,type,var")
+		println("流程控制(15)		go,select,break,case,continue,default,defer,")
+		println("					else,fallthrough,for,goto,if,range,return,switch")
+		println("关键词在程序中具有特殊含义，不可用作变量名或函数名。")
+	}()
+
+	//1-2: Predeclared names
+	func() {
+		println("1-2 预定义标识符")
+		println("----------------")
+		println("除了关键字，Go还预定义若干标识符，在程序任何位置都可以直接使用。按照用途可以分为以下三类：")
+		println("常量(4)		true,false,iota,nil")
+		println("数据类型(20)	int,int8,int16,int32,int64")
+		println("           	uint,uint8,uint16,uint32,uint64,uintptr")
+		println("           	float32,float64,complex64,complex128")
+		println("           	bool,byte,rune,string,error")
+		println("函数(13)		make,len,cap,new,append,copy,close,delete")
+		println("       		complex,real,imag")
+		println("				panic,recover")
+		println("预定义标识符虽然也具有特殊含义，但在实际编程时可以重新定义，覆盖Go语言内置的含义：")
+		println("var true = 1")
+		var true = 1
+		println(true)
+	}()
+
+}
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/Ceifei/learnGo/ch1"
 )
 
 func main() {
@@ -24,6 +26,41 @@ L:
 	//input
 	var input string
 	fmt.Scanln(&input)
+
+	//Chapter 1
+	if input == "1" {
+	L1:
+		println()
+		println("第1章：程序结构")
+		println("-----------------------")
+		fmt.Println("1: 标识符")
+		fmt.Println("2: 声明")
+		fmt.Println("3: 变量")
+		fmt.Println("3: 常量")
+		fmt.Println("0: 返回上一层")
+		println("-----------------------")
+		fmt.Print("请选择: ")
+		//var inp string
+		fmt.Scanln(&input)
+
+		switch input {
+		case "1":
+			ch1.F1()
+		case "2":
+			//datatype.ShowFloat()
+		case "3":
+			//datatype.ShowBool()
+		case "4":
+			//datatype.ShowComplex()
+		case "5":
+			//datatype.ShowString()
+			//datatype.ShowPrintf()
+		default:
+			goto L
+		}
+
+		goto L1
+	}
 
 	//Exit
 	if input == "0" {
